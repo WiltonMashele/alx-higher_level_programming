75123033abe18365bd9c46fc849f17c92c6432d4
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-06 15:42:44 -0700

    fixing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
+for i in range(0, 9):
     for j in range(i + 1, 10):
-        if j == 9:
+        if j == 8:
             print("{}{}".format(i, j), end=" ")
         else:
             print("{}{}".format(i, j), end=", ")
