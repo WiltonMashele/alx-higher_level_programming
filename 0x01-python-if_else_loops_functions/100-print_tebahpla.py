ffe01d97ae4671f649635d4d56a45b53b4c2193d
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-06 16:49:58 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
-print(''.join(["{:c}".format(char if (char % 2 == 0) else (char - 32)) for char in range(122, 96, -1)]))
+output = ''
+for char in range(122, 96, -1):
+    output += "{:c}".format(char if (char % 2 == 0) else char - 32)
+print(output)
