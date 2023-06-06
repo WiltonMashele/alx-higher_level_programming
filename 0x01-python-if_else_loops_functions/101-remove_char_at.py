b968a3d731b37c35b14a3a32ef89292696836d48
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-06 16:42:58 -0700

    101-remove_char_at.py

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,6 @@
+#!/usr/bi/python3
+def remove_char_at(string, index):
+    if index >= 0 and index < len(string):
+        return string[:index] + string[index + 1:]
+    else:
+        return string
