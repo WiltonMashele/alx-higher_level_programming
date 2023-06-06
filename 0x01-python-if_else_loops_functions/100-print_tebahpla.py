a0598c4b60c50520645a33e487b250f9a04d4389
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-06 16:55:56 -0700

    fixing

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
     offset = ord('A') - ord('a') if i % 2 == 0 else 0
-    print(chr(i + ord('a') + offset), end="")
+    print('{}'.format(chr(i + ord('a') + offset)), end="")
