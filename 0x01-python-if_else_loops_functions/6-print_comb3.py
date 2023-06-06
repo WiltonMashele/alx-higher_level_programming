111ea7c32dcfc096241789cfe34baedbf78e210b
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-06 15:32:50 -0700

    fixing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=", " if j < 9 else "\n")
+        if j == 9:
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
