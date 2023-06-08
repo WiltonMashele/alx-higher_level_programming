1fc189e5a878d3ce6a72fa47d53197f365a8cc54
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-08 12:03:13 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
 
-def add_numbers(a, b):
-    return add(a, b)
+def add(a, b):
+    return a + b
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     a = 1
     b = 2
-    result = add_numbers(a, b)
-    print("{:d} + {:d} = {:d}".format(a, b, result))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
