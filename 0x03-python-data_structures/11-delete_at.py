bf16ed889c32d716409a30d83849eda473ee8137
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-11 15:26:05 -0700

    fixing 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        my_list = my_list[:idx] + my_list[idx + 1:]
-    return my_list
+    if my_list is None or idx < 0 or idx >= len(my_list):
+        return my_list
+    return my_list[:idx] + my_list[idx + 1:]
