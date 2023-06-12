b693133a0c5e1f7f1385407524b2f28170797dee
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-12 11:04:27 -0700

    fixing

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(i))
+    i = 0
+    while i < len(my_list):
+        print("{:d}".format(my_list[i]))
+        i = i + 1
