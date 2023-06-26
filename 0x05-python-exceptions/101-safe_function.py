aadd02492dad15ea38f5ab100180d1b5468972bc
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-26 15:44:23 -0700

    fixing

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import traceback
 
 def safe_function(fct, *args):
     """
@@ -9,6 +10,6 @@ def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+    except Exception:
+        traceback.print_exc(file=sys.stderr)
         return None
