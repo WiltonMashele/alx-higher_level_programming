dbdd71319e14499a22a7b291de75d9dc7b0ae4cf
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-26 12:20:34 -0700

    fixing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     cmpt = 0
-    index = 0
-
-    while True:
+    iterator = iter(my_list)
+    
+    while cmpt < x:
         try:
-            if index >= x:
-                break
-            print(my_list[index], end='')
+            print(next(iterator), end='')
             cmpt += 1
-            index += 1
-        except IndexError:
+        except StopIteration:
             break
         except:
             break
-
+    
     print('')
     return cmpt
