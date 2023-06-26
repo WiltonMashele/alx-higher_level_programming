9f16d69f6696f639086b89ec3ad94f9b21aecb2b
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-26 15:36:51 -0700

    fixing

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    result = 0
-
-    if a < 1:
+    try:
+        if a < 1 or b < 1:
+            raise ValueError("Invalid input")
+        result = a ** b / 1 + a ** b / 2
+    except ValueError:
         result = b + a
-    else:
-        result = (a ** b) / (1 + 2)
-
     return result
