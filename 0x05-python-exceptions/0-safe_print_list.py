ea073cddbde3e483fb1a446df94f2b737a529c4b
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-26 12:10:06 -0700

    my first task on Python - Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    cmpt = 0
+    index = 0
+
+    while True:
+        try:
+            if index >= x:
+                break
+            print(my_list[index], end='')
+            cmpt += 1
+            index += 1
+        except IndexError:
+            break
+        except:
+            break
+
+    print('')
+    return cmpt
