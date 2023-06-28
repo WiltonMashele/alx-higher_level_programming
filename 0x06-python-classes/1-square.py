b492a44f37c39d292e6e8097cb407f8501044e03
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-27 18:11:10 -0700

    fixing

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,13 +3,4 @@
 """a class Square that defines a square"""
 
 class Square:
-        """Represent a square."""
-
-    def __init__(self, size):
-        self.size = size
-
-    def area(self):
-        return self.size ** 2
-
-    def perimeter(self):
-        return self.size * 4
+        self._size = size
