c8279d4d87e53b67feea8947585257dc2ceff1f0
WiltonMashele <Wiltonmashele@gmail.com>
2023-06-27 18:19:03 -0700

    fixing

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,5 +6,5 @@
 class Square:
 """a class Square that defines a square"""
     def __init__(self, size):
-        """ Initialize class """
+        """ Initialize square """
         self.__size = size
