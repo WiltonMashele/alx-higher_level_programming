24b13b548162da08f1f5dfb971eee4468a38f377
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-10 17:30:57 -0700

    fixing

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,10 +7,8 @@ Defines the BaseGeometry class, which represents a basic geometric shape.
 class BaseGeometry:
     """Represents the base geometry."""
     def area(self):
-        """Calculates the area of the shape.
-
-        Raises:
-            NotImplementedError: If the method is not overridden in a derived class.
+        """
+        NotImplementedError: If the method is not overridden in a derived class.
         """
         raise NotImplementedError("area() method is not implemented")
 
