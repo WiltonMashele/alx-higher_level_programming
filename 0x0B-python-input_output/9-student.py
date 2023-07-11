2400e011870c717559d1941c4902d7db48c279bb
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 15:49:23 -0700

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module: student.py
+Description: Defines the Student class and its methods.
+"""
+
+
+class Student:
+    """
+    Represents a student with first name, last name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a student instance.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the student.
+        Returns:
+            dict: A dictionary containing the student's attributes.
+        """
+        return {
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'age': self.age
+        }
