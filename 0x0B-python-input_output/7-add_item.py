4581d9b5560e0aefb15df114fc03c7eb9aa1f046
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 16:00:34 -0700

    fixing

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,18 +6,12 @@ The list is then saved to a JSON file.
 import sys
 import json
 
-def save_to_json_file(data, filename):
-    with open(filename, 'w') as file:
-        json.dump(data, file)
-
-def load_from_json_file(filename):
-    with open(filename, 'r') as file:
-        return json.load(file)
-
-if __name__ == "__main__":
+f __name__ == "__main__":
     try:
-        items = load_from_json_file("add_item.json")
+        with open("add_item.json", "r") as file:
+            items = json.load(file)
     except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
-     save_to_json_file(items, "add_item.json")
+    with open("add_item.json", "w") as file:
+        json.dump(items, file)
