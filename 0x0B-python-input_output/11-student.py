43e5330176f37f686bf502eaa845e8f31feefccd
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 15:39:42 -0700

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" 
+Module defining the Student class.
+"""
+
+
+class Student:
+    """ 
+    Class to create instances of students.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ 
+        Initializes a student object.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ 
+        Returns a dictionary representation of the student.
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+
+    def reload_from_json(self, json_data):
+        """ 
+        Replaces all attributes of the student instance.
+        """
+        for attr, value in json_data.items():
+            setattr(self, attr, value)
+
