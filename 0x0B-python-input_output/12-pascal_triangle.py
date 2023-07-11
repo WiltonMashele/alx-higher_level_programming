68637e3e6df87657655389868007a8dfff90519e
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 16:54:13 -0700

    fixing 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-"""This script defines a function to generate Pascal's Triangle."""
+"""
+This script defines a function to generate Pascal's Triangle of a given size.
+"""
 
-def generate_pascals_triangle(n):
-    """Generates Pascal's Triangle of size n.
 
-    Returns a list of lists of integers representing the triangle.
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle of size n.
+
+    Args:
+        n (int): The size of the Pascal's Triangle to generate.
+
+    Returns:
+        list: A list of lists of integers representing Pascal's Triangle.
     """
     if n <= 0:
         return []
 
-    triangle = [[1]]
-    while len(triangle) != n:
-        row = triangle[-1]
-        next_row = [1]
-        for i in range(len(row) - 1):
-            next_row.append(row[i] + row[i + 1])
-        next_row.append(1)
-        triangle.append(next_row)
-    return triangle
+    triangles = [[1]]
+    while len(triangles) != n:
+        current_row = triangles[-1]
+        new_row = [1]
+        for i in range(len(current_row) - 1):
+            new_element = current_row[i] + current_row[i + 1]
+            new_row.append(new_element)
+        new_row.append(1)
+        triangles.append(new_row)
+    return triangles
