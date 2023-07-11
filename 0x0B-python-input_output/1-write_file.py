8ca67b030525b22ee8c27bfbbe5bdf422d9e1b51
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 16:17:28 -0700

    fixing

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Defines a function for appending text to a file."""
+"""Defines a function for writing text to a file."""
 
 
-def append_write(filename="", text=""):
-    """
-    Writes a string to a UTF-8 text file, overwriting the previous contents.
+def write_file(filename="", text=""):
+    """Write a string to a UTF-8 encoded text file.
+
     Args:
-        filename (str): The name of the file to write to.
-        text (str): The string to write to the file.
+        filename (str): The name of the file to write. Default is an empty string.
+        text (str): The text to write to the file. Default is an empty string.
+
     Returns:
         int: The number of characters written.
     """
     with open(filename, "w", encoding="utf-8") as file:
         return file.write(text)
+
