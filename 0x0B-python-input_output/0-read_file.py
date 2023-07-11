099edc1235e2dace359b061d3b29ba077c3fd947
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 13:54:24 -0700

    fixing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,6 @@
 """This program reads a text file and prints its contents to stdout.
 """
 
-
 def read_file(filename=""):
     """
     Reads a text file (UTF-8 encoding) and
@@ -12,7 +11,7 @@ def read_file(filename=""):
         with open(filename, "r", encoding="utf-8") as f:
             contents = f.read()
             print(contents, end="")
-    except FileNotFoundError:
-        print("File not found.")
-    except IOError:
-        print("Error reading the file.")
+    except FileNotFoundError as e:
+        print(f"File '{filename}' not found: {e}")
+    except IOError as e:
+        print(f"Error reading the file '{filename}': {e}")
