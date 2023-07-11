330b16a4e8dc5ce5c36aa51d94f682c65cee47db
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 16:09:19 -0700

    fixing

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,5 +12,5 @@ def append_write(filename="", text=""):
         int: The number of characters written.
     """
 
-    with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+    with open(filename, "w", encoding="utf-8") as file:
+        return file.write(text)
