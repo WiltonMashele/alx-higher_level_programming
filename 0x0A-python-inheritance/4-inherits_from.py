6279cccd18ddefe33a604023d822b2f51f32a344
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-10 17:12:44 -0700

    fixing

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,8 +2,10 @@
 """Module to check class and subclass""
 
 
-def is_kind_of_class(obj, a_class):
-    """Returns True if the object is an instance of the specified class or 
-    any of its subclasses; otherwise, it returns False.
+def inherits_from(obj, a_class):
     """
-    return isinstance(obj, a_class)
+    Returns True if the object is an instance of a class
+    that inherited (directly or indirectly) from the specified class;
+    otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) != a_class
