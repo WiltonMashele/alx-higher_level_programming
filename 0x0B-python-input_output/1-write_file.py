b9eeb50319d2bf07e7ecda8de84fa14721383023
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-11 14:07:58 -0700

    fixing

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -12,13 +12,9 @@ def append_write(filename="", text=""):
         int: The number of characters appended.
     """
     char_count = 0
-    with open(filename, "r+", encoding="utf-8") as f:
-        f.seek(0, 2)
-        start_position = f.tell()
+    with open(filename, "a", encoding="utf-8") as f:
         for char in text:
             f.write(char)
             char_count += 1
-        end_position = f.tell()
-        appended_chars = end_position - start_position
 
-    return appended_chars
+    return char_count
