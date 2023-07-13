5831e91a66fa7dbf54fc520b0342fe2cfa6ecb07
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-12 18:27:15 -0700

    fixing

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,6 +7,5 @@ class MyList(list):
 
     def print_sorted(self):
         """Prints the elements of the list in ascending order."""
-        sorted_list = sorted(self)
         for item in sorted_list:
             print(item)
