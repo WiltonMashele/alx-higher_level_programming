2ff507834e24771403d8fe6fa9d9f3fadee989a1
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-17 16:29:00 -0700

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Base Module"""
+
+class Base:
+    """This module defines a base class."""
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        """Constructor method that assigns the public instance attribute 'id'.
+
+        Args:
+            id (int): An integer value used to manage IDs in this project.
+
+        Returns:
+            None.
+
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
