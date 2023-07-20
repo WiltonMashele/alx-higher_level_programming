dcbf47980b479b724012666243c24ce3c64096d8
WiltonMashele <Wiltonmashele@gmail.com>
2023-07-19 18:04:23 -0700

    updating rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -126,3 +126,4 @@ class Rectangle(Base):
     def to_dictionary(self):
         """Returns the dictionary representation of the Rectangle"""
         return {'id': self.id, 'width': self.width, 'height': self.height, 'x': self.x, 'y': self.y}
+
