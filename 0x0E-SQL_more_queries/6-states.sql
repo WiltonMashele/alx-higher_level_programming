b48d5ba522c67719ccc1f656db71103fb463d594
WiltonMashele <Wiltonmashele@gmail.com>
2023-08-17 04:03:14 -0700

    fixing

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,5 @@
 -- Create states table
 -- Ensure id is auto-generated, non-null, and of integer type.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
