e9cd8afa8f7414afd01a4e5587dee69c01c57289
WiltonMashele <Wiltonmashele@gmail.com>
2023-08-17 13:15:03 -0700

    fixing

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,3 @@
 -- a script that removes all records with a score <= 5 in the table second_table
-UPDATE `second_table`
-SET `score` = 10
-WHERE `name` = "Bob";
+DELETE FROM second_table
+WHERE score <= 5
