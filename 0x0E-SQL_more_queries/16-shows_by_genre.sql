dddf072822de7d0652ecd2107c294251befdaa8d
WiltonMashele <Wiltonmashele@gmail.com>
2023-08-17 03:59:43 -0700

    fixing

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
--- a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 -- Lists all rows from one table that are related to rows in another table
 SELECT title, name AS genre_name
 FROM tv_shows
