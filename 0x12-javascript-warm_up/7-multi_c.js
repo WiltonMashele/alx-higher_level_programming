11eb86d69e30442c30dcb7ca160f8bf4b4046fc3
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-19 13:01:57 -0700

    fixing

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const numOccurrences = parseInt(process.argv[2]);
-if (isNaN(numOccurrences) || numOccurrences < 0) {
+const x = process.argv[2];
+if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-	let i = 0;
-  	while (i < numOccurrences) {
+        let i = 0;
+        while (i < x) {
 		console.log('C is fun');
 		i++;
   }
