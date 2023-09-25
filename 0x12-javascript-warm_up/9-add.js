fde2d0f4c3a46d1addc3ccb209a74ab9aa564120
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-25 13:52:07 -0700

    fixing

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
 function add(a, b) {
-  if (isNaN(a) || isNaN(b)) {
-    console.log('Error: Both arguments must be valid numbers.');
-    return;
-  }
-  console.log(a + b);
+  return a + b;
 }
 
-const num1 = Number(process.argv[2]);
-const num2 = Number(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 
-if (num1 === undefined || num2 === undefined) {
-  console.log('Error: Please provide two numbers as arguments.');
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
 } else {
-  add(num1, num2);
+  console.log(add(a, b));
 }
