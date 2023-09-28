c0e1e970c9d74dbe7c8f892ba4a60d678c9700f1
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-28 15:29:04 -0700

    script that takes in a URL, sends a request to that URL, and displays the size of the body of the response

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | wc -c
+size=$(curl -sI "$1" | wc -c)
+echo "Size of the HTTP response header: $size bytes"
