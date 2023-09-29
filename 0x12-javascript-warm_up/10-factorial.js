c5bfc6a0bff0ef8da5fa7ee35aa2921e51c544ca
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-29 08:20:04 -0700

    fixing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-
 const number = parseInt(process.argv[2]);
-
 const factorial = (num) => {
   if (isNaN(num) || num <= 1) {
     return 1;
   }
   return num * factorial(num - 1);
 };
-
 console.log(factorial(number));
