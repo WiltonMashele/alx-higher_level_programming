a1bd9926008f1c09be80c6f905d23d939e38d82a
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-29 14:27:10 -0700

    fixing

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,21 +4,14 @@
 - sends a request to the URL
 - and displays the body of the response.
 """
-
 import sys
 import requests
 
-def fetch_url(url):
-    try:
-        response = requests.get(url)
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-        else:
-            print(response.text)
-    except requests.RequestException as error:
-        print(f"Failed to fetch URL: {error}")
-
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url_to_fetch = sys.argv[1]
-        fetch_url(url_to_fetch)
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
