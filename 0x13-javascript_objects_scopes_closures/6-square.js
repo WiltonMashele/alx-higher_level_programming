822e3b7d9ef7445ff589bfd983f4ef3a25f91883
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-30 12:32:27 -0700

    fixing

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const SquareParent = require('./5-square.js');
+const SquareP = require('./5-square');
 
-class Square extends SquareParent {
+class Square extends SquareP {
   charPrint(c = 'X') {
     for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+      console.log(`${c.repeat(this.width)}`);
     }
   }
 }
