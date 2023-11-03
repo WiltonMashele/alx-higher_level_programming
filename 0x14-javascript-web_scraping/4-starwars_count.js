fd6cf047491af38ddb45b11301dd2da76ced9445
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-03 11:48:06 -0700

    fixing

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,20 @@
 #!/usr/bin/node
 
-const request = require('request');
+const axios = require('axios');
+const apiUrl = process.argv[2];
+const characterId = process.argv[3];
 
-const apiURL = 'https://swapi-api.alx-tools.com/api/films/';
-const characterId = '18';
-
-request(apiURL, (error, response, body) => {
-  if (!error) {
-    const films = JSON.parse(body).results;
-    const filmsWithWedgeAntilles = films.filter((film) => {
-      return film.characters.some((character) => character.endsWith(`/${characterId}/`));
+if (!apiUrl || !characterId) {
+  console.error('Usage: node script.js <API_URL> <character_id>');
+} else {
+  axios.get(apiUrl)
+    .then((response) => {
+      const nFilms = response.data.results.reduce((count, result) => {
+        return count + result.characters.filter((character) => character.includes(characterId)).length;
+      }, 0);
+      console.log(nFilms);
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error);
     });
-
-    console.log(filmsWithWedgeAntilles.length);
-  }
-});
+}
